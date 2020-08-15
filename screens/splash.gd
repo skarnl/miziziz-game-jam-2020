658862d632d0503277e6a61b37f781749b7023ce
Oskar van Velden <oskar@rakso.nl>
2020-08-15 12:13:38 +0200

    Skip splash when developing

--- screens/splash.gd
+++ screens/splash.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	yield(get_tree().create_timer(3.0), "timeout")
+	if !OS.is_debug_build():
+		yield(get_tree().create_timer(3.0), "timeout")
 	
 	Game.transition_to(Game.GameState.MAIN_MENU)
