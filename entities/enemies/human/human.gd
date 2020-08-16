f2a2e892172a0e642bc37f0afba4e24090cbe3fa
Oskar van Velden <oskar@rakso.nl>
2020-08-16 17:49:14 +0200

    Change possession to proximity

--- entities/enemies/human/human.gd
+++ entities/enemies/human/human.gd
@@ -1,26 +1,68 @@
 extends KinematicBody2D
 
-signal clicked
+signal possessed
+signal hit
 
 var possessed = false setget set_possessed
 var velocity: Vector2 = Vector2.ZERO
 var MAX_SPEED = 70
+enum DIRECTIONS { right, down, left, up }
+var look_directions = {
+	DIRECTIONS.right: 0,
+	DIRECTIONS.down: 90,
+	DIRECTIONS.left: 180,
+	DIRECTIONS.up: 270,
+}
+var player_nearby = false
+
+export(DIRECTIONS) var current_look_direction =  DIRECTIONS.right
 
 func _ready():
-	set_process(false)
-	$Timer.connect('timeout', self, '_on_Timer_timeout')
+	$hint.hide()
 	
-func _on_Timer_timeout():
-#	move()
-	pass
+	set_process(false)
+	set_process_unhandled_input(false)
+	
+	set_look_direction()
+	set_player_detection()
+
+
+func set_look_direction():
+	$Rotation.rotation_degrees = look_directions[current_look_direction]
+
+
+func set_player_detection():
+	$PlayerInteractionArea2D.connect('body_entered', self, '_on_Player_nearby')
+	$PlayerInteractionArea2D.connect('body_exited', self, '_on_Player_away')
+	
+	
+func _on_Player_nearby(body):
+	if body.is_in_group('player'):
+		
+		$hint.show()
+		player_nearby = true
+		set_process_unhandled_input(true)
+		
+
+func _on_Player_away(body):
+	if body.is_in_group('player'):
+		
+		$hint.hide()
+		player_nearby = false
+		set_process_unhandled_input(false)
+	
+
+func _unhandled_input(event):
+	if player_nearby and event is InputEventKey:
+		if event.is_action_pressed('attack'):
+			emit_signal('hit')
+		elif event.is_action_pressed('possess'):
+			emit_signal('possessed')
+
 
 func move(towards:Vector2):
 	position += towards
-
-func _on_Human_input_event(viewport, event, shape_idx):
 	
-	if event is InputEventMouseButton:
-		emit_signal('clicked')
 
 func set_possessed(new_value):
 	possessed = new_value
