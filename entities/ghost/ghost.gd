22ffc0f2736bd3c8a3eb380e82ba12a50078fd8e
Oskar van Velden <oskar@rakso.nl>
2020-08-27 23:13:56 +0200

    Made ghost movement more fluid

--- entities/ghost/ghost.gd
+++ entities/ghost/ghost.gd
@@ -13,15 +13,20 @@ func _ready():
 	$possessedAudioPlayer.play()
 	$moveAudioPlayer.play()
 	
-func _process(delta):
+func get_input_axis():
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
+	return input_vector.normalized()
+	
+func _physics_process(delta):
+	var input_vector = get_input_axis()
+	
+	var ACCELERATION = 1000
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector * MAX_SPEED
+		apply_movement(input_vector * ACCELERATION * delta)
 		
 		$Sprite.flip_h = velocity.x > 0
 		
@@ -30,12 +35,22 @@ func _process(delta):
 		else:
 			$moveAudioPlayer.set_volume_db(0)
 	else:
-		velocity = Vector2.ZERO
+		apply_friction(ACCELERATION * delta)
+		
 		$possessedAudioPlayer.set_volume_db(-80)
 		$moveAudioPlayer.set_volume_db(-80)
 	
-	move_and_slide(velocity)
+	velocity = move_and_slide(velocity)
 	
+func apply_movement(acceleration):
+	velocity += acceleration
+	velocity = velocity.clamped(MAX_SPEED)
+	
+func apply_friction(friction):
+	if velocity.length() > friction:
+		velocity -= velocity.normalized() * friction
+	else:
+		velocity = Vector2.ZERO
 	
 func possess_start(targetPosition):
 	$Tween.interpolate_property(self, 'position', position, targetPosition, 0.3, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
